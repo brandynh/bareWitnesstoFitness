25fd8fd45994feab3038aed880ee40cabbf363e8
Brandyn <hufferbrandyn@gmail.com>
2022-01-19 04:35:20 -0600

    fixes to server.js

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bareWitnesstoFitness', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+//mongoose connecting to database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 //routes
